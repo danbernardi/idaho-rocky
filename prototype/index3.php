e5d1820ce981a74e55257e8688d37e102dc42da7
Daniel Bernardi <dan@zendenwebdesign.com>
2015-03-26 11:02:42 -0700

    makes minor adjustments to index3.php as per clients requests

--- prototype/index3.php
+++ prototype/index3.php
@@ -10,11 +10,8 @@
 	</div>
 	<div class="row">
     <div class="cta-overlay">
-      <h4>Welcome to Idaho Rocky Mountain Ranch,</h4>
+      <h4><span>Welcome to</span>Idaho Rocky Mountain Ranch,</h4>
       <p>The living history of the Old West mingles with the solitude and peace of this 900-acre ranch, offering <strong>freedom</strong>, breathtaking <strong>natural beauty</strong>, and a <strong>relaxed atmosphere.</strong></p>
-      <a class="btn" href="the-experience.php">The Experience</a>
-      <a class="btn" href="gallery.php">The Gallery</a>
-      <a class="btn" href="contact.php">Make a Reservation</a>
     </div>
   </div>
 </section>
